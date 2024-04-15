77495f799b90738af0ed4c2d82b3d83c4e08e9d3
Piotr Kupniewski <kupik59@gmail.com>
2024-04-15 17:33:28 +0200

    a

---
+++ NutritionApp/Migrations/20240415081007_azure1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NutritionApp.Migrations
+{
+    public partial class azure1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- NutritionApp/Program.cs
+++ NutritionApp/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DatabaseContext>(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("AzureConnection")));
 builder.Services.AddHttpClient();
 builder.Services.AddSingleton<FindItemDataStorage>();
 builder.Services.AddDistributedMemoryCache();
